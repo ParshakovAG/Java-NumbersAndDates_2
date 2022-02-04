59ea166d1aaeb0712dccd5108a6892a219197fb7
aleksandr_parshakov <sash29@yandex.ru>
2022-02-04 16:17:06 +0500

    done birthday

--- src/main/java/practice/Birthdays.java
+++ src/main/java/practice/Birthdays.java
@@ -24,16 +24,16 @@ public class Birthdays {
         LocalDate today = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern
                 ("dd.MM.yyyy - E").localizedBy(new Locale("us"));
+        StringBuilder birth = new StringBuilder(birthday);
 
         for (int i = 0; date.isBefore(today) || date.isEqual(today);
-             date = date.plusYears(1)) {
-           birthday =  i++ + " - " + formatter.format(date) + System.lineSeparator();
+            date = date.plusYears(1)) {
+           birth = birth.append(i++ + " - " + formatter.format(date) + System.lineSeparator());
         }
-
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
 
-        return birthday;
+        return birth.toString();
     }
 }

a923a318e59fee56dc4a42c0440a4188c43a2caf
aleksandr_parshakov <sash29@yandex.ru>
2022-02-06 12:11:29 +0500

    done birthday

--- src/main/java/practice/Birthdays.java
+++ src/main/java/practice/Birthdays.java
@@ -1,8 +1,7 @@
 package practice;
+
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 import java.util.Locale;
 
 public class Birthdays {
@@ -13,10 +12,8 @@ public class Birthdays {
         int month = 4;
         int year = 1985;
 
-        //System.out.println(collectBirthdays(year, month, day));
-
+        System.out.println(collectBirthdays(year, month, day));
     }
-
     public static String collectBirthdays(int year, int month, int day) {
 
         String birthday = "";
@@ -27,8 +24,8 @@ public class Birthdays {
         StringBuilder birth = new StringBuilder(birthday);
 
         for (int i = 0; date.isBefore(today) || date.isEqual(today);
-            date = date.plusYears(1)) {
-           birth = birth.append(i++ + " - " + formatter.format(date) + System.lineSeparator());
+             date = date.plusYears(1)) {
+            birth = birth.append(i++ + " - " + formatter.format(date) + System.lineSeparator());
         }
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon

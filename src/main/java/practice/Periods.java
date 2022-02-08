26478dd52bb4f4dc889695e2f03dc74705a71766
aleksandr_parshakov <sash29@yandex.ru>
2022-02-08 20:55:19 +0500

    change Periods

--- src/main/java/practice/Periods.java
+++ src/main/java/practice/Periods.java
@@ -1,14 +1,37 @@
 package practice;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Periods {
 
+    public static void main(String[] args) {
 
-
+        LocalDate today = LocalDate.now();
+        LocalDate javaYear = LocalDate.of(1970, 1, 1);
+        Period dif = Period.between(javaYear, today);
+//        System.out.print("years:" + javaYear.until(today, ChronoUnit.YEARS) + ", ");
+//        System.out.print("months:" +javaYear.until(today, ChronoUnit.MONTHS) + ", ");
+//        System.out.print("days:" + javaYear.until(today, ChronoUnit.DAYS));
+        System.out.println(dif);
+    }
     // реализуйте вывод разницы между датами, используя класс Period
     public static String getPeriodFromBirthday(LocalDate firstDate, LocalDate secondDate) {
-        return "";
+
+        String text = "";
+        LocalDate today = LocalDate.now();
+        LocalDate javaYear = LocalDate.of(1970, 1, 1);
+
+        Period dif = Period.between(javaYear, today);
+
+        StringBuilder df = new  StringBuilder(text);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("years: , months: , days: ");
+        df = df.append(" - " + formatter.format(today) + System.lineSeparator());
+        System.out.println(df);
+
+        return text.toString();
     }
 
 }

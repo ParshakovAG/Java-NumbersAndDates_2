89ea89df897894ae3828aae654917da8f1ac751f
aleksandr_parshakov <sash29@yandex.ru>
2022-02-17 20:33:20 +0500

    Done Periods

--- src/main/java/practice/Periods.java
+++ src/main/java/practice/Periods.java
@@ -9,24 +9,21 @@ public class Periods {
 
     public static void main(String[] args) {
 
-        LocalDate today = LocalDate.now();
-        LocalDate javaYear = LocalDate.of(1970, 1, 1);
+        LocalDate firstDay = LocalDate.now();
+        LocalDate secondDate = LocalDate.of(1970, 1, 1);
 
-        System.out.println(getPeriodFromBirthday(javaYear, today));
+        System.out.println(getPeriodFromBirthday(firstDay, secondDate));
     }
     // реализуйте вывод разницы между датами, используя класс Period
     public static String getPeriodFromBirthday(LocalDate firstDate, LocalDate secondDate) {
 
         String text = "";
-        LocalDate today = LocalDate.of(1995, 5, 23);
-        LocalDate javaYear = LocalDate.of(2021, 11, 1);
 
-        Period dif = Period.between(today, javaYear);
-        StringBuilder dtf = new  StringBuilder(text);
+        Period dif = Period.between(firstDate, secondDate);
 
-        dtf = dtf.append("years:" + dif.getYears() + ", months:" + dif.getMonths() + ", days:" + dif.getDays());
+        StringBuilder df = new StringBuilder(text);
+        df = df.append("years:" + dif.getYears() + ", months:" + dif.getMonths() + ", days:" + dif.getDays());
 
-        return dtf.toString();
+        return df.toString();
     }
-
 }
